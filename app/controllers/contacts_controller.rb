fb8c75ac1e0192b25cf5a797845ee9c34d7f067c
Peter Sonneveld <peter_tjuuh@hotmail.com>
2014-06-17 22:09:04 +0200

    Further changes to contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.save
-      redirect_to '/contacts', notice: 'Thank you for your message. We will contact you soon!'
+      redirect_to root_path, notice: 'Thank you for your message. We will contact you soon!'
     else
       render :new, error: 'Cannot send message.'
     end
