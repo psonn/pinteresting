11542a0495bd1e3f022947725aa7acbb3ff5bdc8
Peter Sonneveld <peter_tjuuh@hotmail.com>
2014-06-17 22:05:30 +0200

    Further changes to contact page design

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,11 +6,10 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    if @contact.save
+      redirect_to '/contact', notice: 'Thank you for your message. We will contact you soon!'
     else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
+      render :new, error: 'Cannot send message.'
     end
   end
 end
