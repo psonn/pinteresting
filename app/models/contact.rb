625d5cbbacd284d06e2583dbb610d694678d716e
Peter Sonneveld <peter_tjuuh@hotmail.com>
2014-06-17 21:16:18 +0200

    Added a contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
+      :subject => "Message from #{name}",
       :to => "info@doamer.com",
       :from => %("#{name}" <#{email}>)
     }
